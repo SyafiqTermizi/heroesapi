c786ed0f02a94c4e1c2a0def16b77bd918a5a130
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2018-04-01 14:53:24 +0800

    create hero model

--- heroesapi/main/models.py
+++ heroesapi/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Hero(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
