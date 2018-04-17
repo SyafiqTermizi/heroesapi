fad1dbc85f3afd0fe5715411a1938dd78acc88b9
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2018-04-17 15:26:49 +0800

    changes

--- heroesapi/main/models.py
+++ heroesapi/main/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 
 class Hero(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=250, blank=True)
